b8d19683d029453357f16b04a579570be6596031
quangdm1702 <doanminhquang17022003@gmail.com>
2024-11-20 14:06:28 +0700

    toastify

--- i18next/src/main.tsx
+++ i18next/src/main.tsx
@@ -2,14 +2,20 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./services/i18n/index.ts";
+import { RecoilRoot } from "recoil";
+import RecoilDemo from "./samples/RecoilDemo.tsx";
 import I18nDemo from "./samples/I18nDemo.tsx";
+import { ToastContainer } from "react-toastify";
+import ToastifyDemo from "./samples/ToastifyDemo.tsx";
 
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <I18nDemo />
+    <RecoilRoot>
+      <I18nDemo />
+      <RecoilDemo />
+      <ToastifyDemo />
+    </RecoilRoot>
+    <ToastContainer />
   </StrictMode>
 );
-
-// Import components I18nDemo vào thẻ div có id là root để render ra giao diện
-// Import "./services/i18n/index.ts"; cấu hình i18next để hỗ trợ đa ngôn ngữ , đảm bảo rằng i18next được kích hoạt trước khi ứng dụng render
